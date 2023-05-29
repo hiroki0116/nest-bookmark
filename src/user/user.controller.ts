dda4c155ebcd402214afad2c56740f05fda0150f
hiroki0116 <hirokiseino0116@gmail.com>
2023-05-30 02:51:51 +1000

    Fix import path

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseGuards, Body, Patch } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetUser } from '../../src/auth/decorator';
+import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
 import { EditUserDto } from './dto';
 import { UserService } from './user.service';
